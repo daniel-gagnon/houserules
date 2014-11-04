460bddb354d7f998e75a9fbc3118d8bd90015eb8
Daniel Gagnon <dan@daniel-gagnon.me>
2014-11-04 00:43:16 -0500

    Close db connection on shutdown

--- src/houserules/database.clj
+++ src/houserules/database.clj
@@ -4,6 +4,9 @@
             [bitemyapp.revise.core :refer [run run-async]]))
 
 (def ^:private conn (connect))
+(.addShutdownHook
+  (Runtime/getRuntime)
+  (Thread. #(close conn)))
 
 (defmacro with-db [& bodies]
   `(-> (r/db "houserules") ~@bodies (run conn)))
