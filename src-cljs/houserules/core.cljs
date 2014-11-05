e2edb0131d8802fc17503aba4dfeb4ee40f7392c
Daniel Gagnon <dan@daniel-gagnon.me>
2014-11-05 00:44:39 -0500

    Implemented client side login

--- src-cljs/houserules/core.cljs
+++ src-cljs/houserules/core.cljs
@@ -1,12 +1,28 @@
 (ns houserules.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [houserules.routes :refer [current-page]]))
+            [houserules.routes :refer [current-page]]
+            [houserules.login :refer [logged-in?]]))
+
+(defn sign-in-out
+  "Sign in or out section"
+  []
+  [:nav.right.menu
+   (if (logged-in?)
+     [:a.item {:on-click #(.logout (.-id js/navigator))} [:i.sign.out.icon] "Sign out"]
+     [:a.item {:on-click #(.request (.-id js/navigator))} [:i.sign.in.icon] "Sign in"])])
+
+(defn top-nav
+  "Navigation on top"
+  []
+  [:nav.ui.menu [:a.active.item [:i.home.icon] "Home"]  [sign-in-out]])
 
 (defn site
   "The whole site"
   []
-  (case (current-page)
-    :home [:h1 "Home"]))
+  [:div
+   [top-nav]
+   (case (current-page)
+     :home [:h1 "Home"])])
 
 (defn init! []
   (reagent/render-component [site] (.getElementById js/document "app")))
---
+++ src-cljs/houserules/login.cljs
@@ -0,0 +1,19 @@
+(ns houserules.login
+  (:require [ajax.core :refer [POST]]))
+
+(def ^:private logged-in (atom false))
+
+(defn logged-in? [] @logged-in)
+
+(defn- on-login [assertion]
+  (POST "/auth/login"
+        {:params {:assertion assertion}
+         :handler #(reset! logged-in true)
+         :error-handler #(do (js/alert "Login eror") (.log js/console %) (.logout (.-id js/navigator)))}))
+
+(defn- on-logout []
+  (POST "/auth/logout"
+        {:handler #(reset! logged-in false)
+         :error-handler #(do (js/alert "Logout eror") (.log js/console %))}))
+
+(.watch (.-id js/navigator) (js-obj "onlogin" on-login, "onlogout" on-logout))
