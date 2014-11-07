6b10f3e26b157f02dd03abe58cdcbd4d8cb09bdd
Daniel Gagnon <dan@daniel-gagnon.me>
2014-11-07 00:17:18 -0500

    Implemented transactions

--- src/houserules/database/bdb.clj
+++ src/houserules/database/bdb.clj
@@ -1,10 +1,12 @@
 (ns houserules.database.bdb
   (:require [taoensso.nippy :as nippy])
-  (:import [com.sleepycat.je Environment EnvironmentConfig DatabaseConfig DatabaseEntry]
+  (:import [com.sleepycat.je Environment EnvironmentConfig DatabaseConfig DatabaseEntry Transaction]
            [java.io File]))
 
 (def ^:private shutting-down (ref false))
 (def ^:private in-flight (ref 0))
+
+(def ^:private ^:dynamic *transaction* nil)
 
 (def ^:private environment
   (delay
@@ -26,6 +28,26 @@
 
 (defn entry->clj [entry]
   (nippy/thaw (.getData entry)))
+
+(defn commit [trx]
+  (.commit trx)
+  (dosync (commute in-flight dec)))
+
+(defn abort [trx]
+  (.abort trx)
+  (dosync (commute in-flight dec)))
+
+(defmacro with-transaction [& body]
+  "Wraps in a transaction, adds a commit at the end if no commit is present"
+  `(when (dosync
+           (ensure shutting-down)
+           ; Child transaction can be acquired even when the app is shutting down but not parent transactions
+           (when (or *transaction* (not @shutting-down))
+             (commute in-flight inc)))
+
+     (binding [*transaction* (.beginTransaction @environment *transaction* nil)]
+       ~@body
+       ~@(when (not (some #{commit} (flatten body))) [commit]))))
 
 (.addShutdownHook (Runtime/getRuntime) (Thread. #(dosync (ref-set shutting-down true))))
 (.addShutdownHook
