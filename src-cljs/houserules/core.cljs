81553437b73b8636fb3df319a3f16e03c991e775
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-12 00:24:16 -0500

    Refactored topnav into its own namespace

--- src-cljs/houserules/core.cljs
+++ src-cljs/houserules/core.cljs
@@ -1,30 +1,7 @@
 (ns houserules.core
   (:require [reagent.core :as reagent :refer [atom]]
             [houserules.routes :refer [current-page]]
-            [houserules.login :refer [email full-name admin?]]))
-
-(defn sign-out-text []
-  [:span "Sign out (" (or @email @full-name) ")"])
-
-(defn sign-in-out
-  "Sign in or out section"
-  []
-  (if @email
-    [:a.item {:on-click #(.logout (.-id js/navigator))} [:i.sign.out.icon] [sign-out-text]]
-    [:a.item {:on-click #(.request (.-id js/navigator))} [:i.sign.in.icon] "Sign in"])
-  )
-
-(defn admin []
-  (when @admin?
-    [:a.item [:i.settings.icon] "Admin"]))
-
-(defn top-nav
-  "Navigation on top"
-  []
-  [:nav.ui.menu [:a.active.item [:i.home.icon] "Home"]
-   [:nav.right.menu
-    [admin]
-    [sign-in-out]]])
+            [houserules.topnav :refer [top-nav]]))
 
 (defn site
   "The whole site"
