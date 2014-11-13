c7b3fcfd50cf21e14b14ddac9289c883a7a19c75
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-13 01:21:31 -0500

    Minifying css for prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
 
 gulp.task('sass', function () {
     return gulp.src('resources/public/scss/*.scss')
-        .pipe(sass({sourcemap: true, sourcemapPath: '.'}))
+        .pipe(sass())
         .on('error', function (err) { console.log(err.message); })
         .pipe(gulp.dest('resources/public/css/'));
 });
@@ -19,6 +21,13 @@ gulp.task('prefix', ['sass'], function() {
 
 gulp.task('watch', function() {
     gulp.watch('resources/public/scss/*.scss', ['sass']);
+});
+
+gulp.task('prod', ['prefix'], function() {
+    return gulp.src('resources/public/css/semantic.css', 'resources/public/css/styles.css')
+        .pipe(concat('styles.min.css'))
+        .pipe(csso())
+        .pipe(gulp.dest('resources/public/css/'));
 });
 
 gulp.task('default', ['sass', 'prefix'], function() {});
