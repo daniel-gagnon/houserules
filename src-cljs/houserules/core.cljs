54a6f4a7526963f5b0018ec5b9f3e185625357ee
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-12 20:58:53 -0500

    Added welcome message and profile function

--- src-cljs/houserules/core.cljs
+++ src-cljs/houserules/core.cljs
@@ -1,18 +1,28 @@
 (ns houserules.core
   (:require [reagent.core :as reagent :refer [atom]]
+            [houserules.login :refer [email]]
             [houserules.routes :refer [current-page]]
-            [houserules.topnav :refer [top-nav]]))
+            [houserules.topnav :refer [top-nav]]
+            [houserules.pages.profile :refer [profile]]))
+
+(defn welcome-message []
+  [:div.ui.message
+   [:div.header "Welcome to Houserules"]
+   [:p "Houserules is a tool to manage your LARPs and LARPing with the White Wolf™ franchises."]
+   [:p "Please log in with a valid e-mail address."]])
 
 (defn site
   "The whole site"
   []
   [:div
    [top-nav]
-   (case @current-page
-     :home [:h1 "Home"]
-     :profile [:h1 "Profile"]
-     :admin [:h1 "Admin"]
-     nil)])
+   (if @email
+     (case @current-page
+       :home [:h1 "Home"]
+       :profile [profile]
+       :admin [:h1 "Admin"]
+       nil)
+     [welcome-message])])
 
 (defn init! []
   (reagent/render-component [site] (.getElementById js/document "app")))
---
+++ src-cljs/houserules/pages/profile.cljs
@@ -0,0 +1,5 @@
+(ns houserules.pages.profile
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(defn profile []
+  [:h1.ui.header "Profile"])
