d3ca64ee2be7995b2132d650d55fd3a629b0aa5b
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-13 19:39:22 -0500

    Added build to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var csso = require('gulp-csso');
+var exec = require('child_process').exec;
 
 gulp.task('sass', function () {
     return gulp.src('resources/public/scss/*.scss')
@@ -23,11 +24,21 @@ gulp.task('watch', function() {
     gulp.watch('resources/public/scss/*.scss', ['sass']);
 });
 
-gulp.task('prod', ['prefix'], function() {
+gulp.task('minify', ['prefix'], function() {
     return gulp.src('resources/public/css/semantic.css', 'resources/public/css/styles.css')
         .pipe(concat('styles.min.css'))
         .pipe(csso())
         .pipe(gulp.dest('resources/public/css/'));
 });
+
+
+gulp.task('build', ['minify'], function (cb) {
+  exec('lein ring uberjar', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
 
 gulp.task('default', ['sass', 'prefix'], function() {});
