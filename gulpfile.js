73e3ba1b405f4480188b785460f11d892dc8f042
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-13 19:58:18 -0500

    Cleaning before builds

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var csso = require('gulp-csso');
 var exec = require('child_process').exec;
+var rimraf = require('rimraf');
 
 gulp.task('sass', function () {
     return gulp.src('resources/public/scss/*.scss')
@@ -31,14 +32,20 @@ gulp.task('minify', ['prefix'], function() {
         .pipe(gulp.dest('resources/public/css/'));
 });
 
+gulp.task('clean', function() {
+    rimraf.sync('./target');
+    rimraf.sync('./resources/public/css')
+    rimraf.sync('./resources/public/js')
+    rimraf.sync('./.sass-cache')
+});
 
-gulp.task('build', ['minify'], function (cb) {
+
+gulp.task('build', ['clean', 'minify'], function (cb) {
   exec('lein ring uberjar', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 })
-
 
 gulp.task('default', ['sass', 'prefix'], function() {});
