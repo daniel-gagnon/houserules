31152f6a1d9a6279b73d9f7a0d21dbfbd36237a6
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-15 17:27:28 -0500

    Reading email settings

--- src/houserules/email.clj
+++ src/houserules/email.clj
@@ -1,9 +1,28 @@
 (ns houserules.email
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clj-yaml.core :as yaml]
+            [slingshot.slingshot :refer [try+]])
+  (:import [java.io FileNotFoundException]))
 
-(def settings (atom nil))
+(def owner (atom nil))
+(def system (atom nil))
+(def host (atom nil))
+(def port (atom nil))
+(def username (atom nil))
+(def password (atom nil))
 
-(defn read-settings [] false)
+(defn read-settings []
+  (try+
+    (let [data (yaml/parse-string (slurp "settings.yaml"))]
+      (reset! owner (get-in data [:e-mail :owner]))
+      (reset! system (get-in data [:e-mail :system]))
+      (reset! host (get-in data [:smtp :host]))
+      (reset! port (get-in data [:smtp :port]))
+      (reset! username (get-in data [:smtp :username]))
+      (reset! password (get-in data [:smtp :password]))
+      true)
+    (catch FileNotFoundException _ false)))
+
 
 (defn write-default-settings []
   (spit "settings.yaml" (slurp  (io/resource "default-settings.yaml"))))
