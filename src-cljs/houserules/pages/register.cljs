56145d761e7b51397918fce18ed8366c5d9312e4
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-15 12:17:28 -0500

    Added thank you for registering

--- src-cljs/houserules/pages/register.cljs
+++ src-cljs/houserules/pages/register.cljs
@@ -1,11 +1,20 @@
 (ns houserules.pages.register
   (:require [reagent.core :as reagent :refer [atom]]))
 
+(defn fill-email [email-atom]
+  [:div.ui.form.attached.fluid.segment
+   [:input {:placeholder "e-mail" :auto-focus true :on-key-down #(.log js/console (.-keyCode %))}]
+   [:button.ui.green.button "Register"]])
+
+(defn thank-you [email]
+  [:div.ui.form.attached.fluid.segment
+   [:p "Click the link that was sent at " email " to activate your account."]])
+
 (defn register []
-  [:div#register
-   [:div.ui.attached.message
-    [:div.header "Register"]
-    [:p "All you need is a valid e-mail address"]]
-   [:div.ui.form.attached.fluid.segment
-    [:input {:placeholder "e-mail" :auto-focus true :on-key-down #(.log js/console (.-keyCode %))}]
-    [:button.ui.green.button "Register"]]])
+  (let [email (atom nil)]
+    (fn []
+      [:div#register
+       [:div.ui.attached.message
+        [:div.header (if (not @email) "Register" "Thank you")]
+        (when-not @email [:p "All you need is a valid e-mail address"])]
+       (if (not @email) [fill-email email] [thank-you @email])])))
