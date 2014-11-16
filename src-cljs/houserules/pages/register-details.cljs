4c826445ee877aa344d6b3ed80debf3bca4dffe9
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-16 02:27:12 -0500

    Added name input for registration

--- src-cljs/houserules/pages/register-details.cljs
+++ src-cljs/houserules/pages/register-details.cljs
@@ -1,7 +1,12 @@
 (ns houserules.pages.register-details
   (:require [reagent.core :as reagent :refer [atom]]
-            [ajax.core :refer [POST]]))
+            [ajax.core :refer [POST]]
+            [houserules.login :refer [full-name]]))
 
 (defn register-details []
   [:div#register-details
-   [:h1 "Register details"]])
+   [:div.ui.attached.message
+    [:div.header "Registration"]
+    [:p "Please complete your registration."]]
+   [:div.ui.form.attached.fluid.segment
+    [:input {:placeholder "Name" :on-change #(reset! full-name (let [n (-> % .-target .-value)] (when (not= n "") n)) )}]]])
