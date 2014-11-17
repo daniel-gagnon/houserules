8579f76bdec2ae75579abe7e8b437c87cbd79329
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-16 22:15:43 -0500

    Removing messages works

--- src-cljs/houserules/messages.cljs
+++ src-cljs/houserules/messages.cljs
@@ -4,13 +4,16 @@
 (def msgs (atom []))
 
 (defn messages []
-  [:div (when (pos? (count @msgs)) @msgs)])
+  [:div (for [msg @msgs] msg)])
+
+(defn remove-message [id]
+  (swap! msgs (fn [coll] (filter #(not= (:id (meta %)) id) coll))))
 
 (defn add-message [type header content]
   (let [id (gensym)]
     (swap! msgs conj ^{:id id}
                      [(keyword (str "div.ui.message." (name type)))
-                      [:i.close.icon {:on-click (swap! msgs (fn [coll] (filter #(not= (:id (meta %)) id) coll)))}]
+                      [:i.close.icon {:on-click #(remove-message id)}]
                       (when header
                         [:div.header header])
                       content])))
