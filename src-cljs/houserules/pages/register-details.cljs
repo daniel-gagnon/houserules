ebcf94a7fec40366a9d2dbb107a4c5b7d0cb754a
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-16 19:47:23 -0500

    Password strength bar working

--- src-cljs/houserules/pages/register-details.cljs
+++ src-cljs/houserules/pages/register-details.cljs
@@ -1,33 +1,32 @@
 (ns houserules.pages.register-details
   (:require [reagent.core :as reagent :refer [atom]]
             [ajax.core :refer [POST]]
-            [houserules.login :refer [full-name invalid-token?]]))
+            [houserules.login :refer [full-name invalid-token? email]]))
 
 (defn password-strength [strength]
-  [:div.ui.red.bottom.attached.progress
-   [:div.bar {:height "0.25em"}]
+  [:div.ui.bottom.attached.progress
+   [:div.bar {:style {:background-color (["white" "red" "orange" "yellow" "green"] strength) :width (str (* strength 25) "%")}}]
    [:span "Password strength: " (["Terrible" "Weak" "So-so" "Good" "Strong"] strength)]] )
 
-;<div class="ui bottom attached progress">
-;<div class="bar"></div>
-;</div>
-
-(defn compute-strength [password] 0)
+(defn compute-strength [password]
+  (let [zxcvbn-score (js/zxcvbn password (js/Array @full-name @email))]
+    (.-score zxcvbn-score)))
 
 (defn register-details []
-  [:div#register-details
-   [:div.ui.attached.message
-    [:div.header (if-not @invalid-token? "Registration" "Invalid token")]
-    [:p (if-not @invalid-token? "Please complete your registration." "Your token is invalid or has expired.")]]
-   [:div.ui.form.attached.fluid.segment
-    (if-not @invalid-token?
-      (let [password (atom "")
-            strength (atom 0)]
-        (add-watch password (gensym) (fn [_ _ _ pass] (reset! strength (compute-strength pass))))
-        [:div.ui.form
-         [:input {:placeholder "Name" :auto-focus true :on-change #(reset! full-name (let [n (-> % .-target .-value)] (when (not= n "") n)) )}]
-         [:input {:placeholder "Password" :type :password}]
-         [password-strength @strength]
-         [:a {:href "http://xkcd.com/936/" :target "_blank"} "Advices for picking a strong and easy to remember password"]
-         [:button.ui.green.button "Complete Registration"]])
-      [:p "Please try registering again."])]])
+  (let [password (atom "")
+        strength (atom 0)]
+    (add-watch password (gensym) (fn [_ _ _ pass]  (reset! strength (compute-strength pass))))
+    (fn []
+      [:div#register-details
+       [:div.ui.attached.message
+        [:div.header (if-not @invalid-token? "Registration" "Invalid token")]
+        [:p (if-not @invalid-token? "Please complete your registration." "Your token is invalid or has expired.")]]
+       [:div.ui.form.attached.fluid.segment
+        (if-not @invalid-token?
+          [:div.ui.form
+           [:input {:placeholder "Name" :auto-focus true :on-change #(reset! full-name (let [n (-> % .-target .-value)] (when (not= n "") n)) )}]
+           [:input {:placeholder "Password" :type :password :on-change #(reset! password (-> % .-target .-value))}]
+           [password-strength @strength]
+           [:a {:href "http://xkcd.com/936/" :target "_blank"} "Advices for picking a strong and easy to remember password"]
+           [:button.ui.green.button "Complete Registration"]]
+          [:p "Please try registering again."])]])))
