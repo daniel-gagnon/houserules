7f69850a194bfe8feb80693a4df1d3fc3b42ed10
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-17 21:38:08 -0500

    Displaying an error when someone was already registered

--- src-cljs/houserules/pages/register.cljs
+++ src-cljs/houserules/pages/register.cljs
@@ -1,26 +1,42 @@
 (ns houserules.pages.register
   (:require [reagent.core :as reagent :refer [atom]]
-            [houserules.ajax :refer [POST]]))
+            [houserules.ajax :refer [POST]]
+            [clojure.string :as string]))
 
-(defn- fill-email [email-atom]
+(defn send-registration [data email in-flight already-registered?]
+  (reset! in-flight true)
+  (POST "/auth/register"
+        {:params {:email data}
+         :handler (fn [response]
+                    (reset! in-flight false)
+                    (reset! already-registered? (not response))
+                    (when-not @already-registered? (reset! email data)))}))
+
+(defn- fill-email [_ _ _]
   (let [data (atom "")]
-    [:div.ui.form.attached.fluid.segment
-     [:input {:placeholder "e-mail" :auto-focus true :on-change #(reset! data (-> % .-target .-value)) :on-key-down #(when (= 13 (.-keyCode %)) (reset! email-atom @data))}]
-     [:button.ui.green.button {:on-click #(reset! email-atom @data)} "Register"]]))
+    (fn [email in-flight already-registered?]
+      [:div.ui.form.attached.fluid.segment
+       [:input {:placeholder "e-mail" :auto-focus true :disabled @in-flight :on-change #(reset! data (-> % .-target .-value)) :on-key-down #(when (= 13 (.-keyCode %)) (reset! email @data))}]
+       [(if-not (or @in-flight (= (string/trim @data) "")) :button.ui.green.button :button.ui.green.button.disabled) {:disabled (or @in-flight (= (string/trim @data) "")) :on-click #(do (send-registration (string/trim @data) email in-flight already-registered?))} "Register"]]
+      )
+    ))
 
 (defn- thank-you [email]
   [:div.ui.form.attached.fluid.segment
    [:p "Click the link that was sent at " email " to activate your account."]])
 
+(defn- already-registered []
+  [:div.ui.form.attached.fluid.segment
+   [:p "You can ask for a password reset e-mail on the \"Sign In\" page."]])
+
+
 (defn register []
-  (let [email (atom nil)]
-    (add-watch email (gensym)
-               (fn [_ _ _ email]
-                 (POST "/auth/register"
-                       {:params {:email email}})))
+  (let [email (atom nil)
+        in-flight (atom false)
+        already-registered? (atom false)]
     (fn []
       [:div#register
        [:div.ui.attached.message
-        [:div.header (if (not @email) "Register" "Thank you")]
-        (when-not @email [:p "All you need is a valid e-mail address"])]
-       (if (not @email) [fill-email email] [thank-you @email])])))
+        [:div.header (cond @already-registered? "Already registered" (not @email) "Register" :else "Thank you")]
+        (if @already-registered? [:p "Did you forget your password?"] (when-not @email [:p "All you need is a valid e-mail address"]))]
+       (cond @already-registered? [already-registered] (not @email) [fill-email email in-flight already-registered?] :else [thank-you @email])])))
