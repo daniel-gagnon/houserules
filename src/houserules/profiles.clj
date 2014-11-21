38fc8255e98a68e1f08d66d2ad562f678eeb146b
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-21 00:00:59 -0500

    Encrypting the passwords

--- src/houserules/profiles.clj
+++ src/houserules/profiles.clj
@@ -1,6 +1,7 @@
 (ns houserules.profiles
   (:require [houserules.database.bdb :refer [db-get put with-transaction with-database]]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [noir.util.crypt :refer [encrypt]]))
 
 (defn update-profile [options]
   (with-transaction
@@ -8,4 +9,8 @@
       :users
       (let [email (session/get :email)]
         (put email
-             (merge (db-get email) options))))))
+             (merge
+               (db-get email)
+               (if (:password options)
+                 (update-in options [:password] encrypt)
+                 options)))))))
