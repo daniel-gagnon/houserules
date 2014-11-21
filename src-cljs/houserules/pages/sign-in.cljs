5ecbc9cd0ba5123d9ff42c1e6f3a10cdaba4c78c
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-20 21:34:38 -0500

    Added sign-in form

--- src-cljs/houserules/pages/sign-in.cljs
+++ src-cljs/houserules/pages/sign-in.cljs
@@ -1,5 +1,19 @@
 (ns houserules.pages.sign-in
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :as string]))
+
+(defn- send-login [email password in-flight])
 
 (defn sign-in []
-  [:h1 "Sign In"])
+  (let [email (atom "")
+        password (atom "")
+        in-flight (atom false)
+        disable-button #(or @in-flight (some string/blank? [@email @password]))]
+    (fn []
+      [:div#sign-in
+       [:div.ui.attached.message
+        [:div.header "Sign in"]]
+       [:div.ui.form.attached.fluid.segment
+        [:input.ui.input {:placeholder "Email" :disabled @in-flight :auto-focus true :on-change #(reset! email (string/trim (-> % .-target .-value)))}]
+        [:input.ui.input {:placeholder "Password" :type :password :disabled @in-flight :on-change #(reset! password (-> % .-target .-value))}]
+        [(if-not (disable-button) :button.ui.green.button :button.ui.green.button.disabled) {:disabled (disable-button) :on-click #(do (send-login email password in-flight))} "Login"]]])))
