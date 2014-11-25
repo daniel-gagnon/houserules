2bd7647c1ebeb90cd87f0c3785f520b1941ab22e
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-25 01:18:06 -0500

    Added function to send password reset emails

--- src/houserules/email.clj
+++ src/houserules/email.clj
@@ -11,12 +11,15 @@
         hex (sha1-sign-hex secret-key (str email tomorrow))]
     (str email \~ tomorrow \~ hex)))
 
+(defn smtp-auth []
+  {:host @host
+   :user @username
+   :port @port
+   :pass @password})
+
 (defn send-registration-email [email]
   (let [link (str @domain "/register/" (make-token email))
-        smtp-auth {:host @host
-                   :user @username
-                   :port @port
-                   :pass @password}
+        auth (smtp-auth)
         message {:from @system
                  :to email
                  :subject "Please confirm your Houserules registration"
@@ -26,4 +29,18 @@
                                       (.replace "{email}" email)
                                       (.replace "{link}" link)
                                       md-to-html-string)}]}]
-    (send-message smtp-auth message)))
+    (send-message auth message)))
+
+(defn send-password-reset-email [email]
+  (let [link (str @domain "/password-reset/" (make-token email))
+        auth (smtp-auth)
+        message {:from @system
+                 :to email
+                 :subject "Houserules password reset"
+                 :body [{:type "text/html; charset=utf-8"
+                         :content (-> (io/resource "email-templates/password-reset.txt")
+                                      slurp
+                                      (.replace "{email}" email)
+                                      (.replace "{link}" link)
+                                      md-to-html-string)}]}]
+    (send-message auth message)))
