1b80c7732d527d146e6b6b820da6511a6189f6bf
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-25 23:13:36 -0500

    Display if email or password is wrong

--- src-cljs/houserules/pages/sign-in.cljs
+++ src-cljs/houserules/pages/sign-in.cljs
@@ -5,31 +5,34 @@
             [houserules.login :refer [register-user]]
             [houserules.routes :refer [navigate-to home-route]]))
 
-(defn- send-login [email password in-flight error]
+(defn- send-login [email password in-flight email-error password-error]
   (reset! in-flight true)
   (POST "/auth/login"
     {:params {:email email
               :password password}
      :handler #(do (register-user %) (navigate-to (home-route)))
-     :error-handler #(do (reset! in-flight false) (reset! error true))}))
+     :error-handler #(do (reset! in-flight false)
+                         (reset! (case (:error (:response %))
+                                  :user-doesn't-exist email-error
+                                  :password-error password-error) true))}))
 
 (defn sign-in []
   (let [email (atom "")
         password (atom "")
         in-flight (atom false)
         disable-button #(or @in-flight (some string/blank? [@email @password]))
-        error (atom false)
-        login #(do (send-login @email @password in-flight error))]
+        email-error (atom false)
+        password-error (atom false)
+        login #(do (send-login @email @password in-flight email-error password-error))
+        clear-errors #(do (reset! email-error false) (reset! password-error false))]
     (fn []
-      [:div#sign-in.ui.error.form
+      [:div#sign-in.ui.form
        [:div.ui.attached.message
         [:div.header "Sign in"]]
        [:div.ui.form.attached.fluid.segment
-        (when @error
-          [:div.ui.error.message
-           [:div.header "Error"]
-           [:p "Email or password incorrect"]])
-        [:input#email.ui.input {:value @email :placeholder "Email" :disabled @in-flight :auto-focus true :on-change #(do (reset! error false) (reset! email (string/trim (-> % .-target .-value))))}]
-        [:input#password.ui.input {:value @password :placeholder "Password" :type :password :disabled @in-flight :on-change #(do (reset! error false) (reset! password (-> % .-target .-value))) :on-key-down #(when (= 13 (.-keyCode %)) (login))}]
+        [(if-not @email-error :div.field :div.field.error)
+         [:input#email.ui {:value @email :placeholder "Email" :type :text :disabled @in-flight :auto-focus true :on-change #(do (clear-errors) (reset! email (string/trim (-> % .-target .-value))))}]]
+        [(if-not @password-error :div.field :div.field.error)
+         [:input#password.ui {:value @password :placeholder "Password" :type :password :disabled @in-flight :on-change #(do (clear-errors) (reset! password (-> % .-target .-value))) :on-key-down #(when (= 13 (.-keyCode %)) (login))}]]
         [(keyword (str "button#forgot-password.ui.button" (when (or (not (re-find #".+@.+\..+" @email)) @in-flight) ".disabled"))) {:disabled (or (not (re-find #".+@.+\..+" @email)) @in-flight)} "I forgot my password"]
         [(if-not (disable-button) :button.ui.primary.button :button.ui.primary.button.disabled) {:disabled (disable-button) :on-click login} "Login"]]])))
