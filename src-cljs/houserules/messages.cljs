3268a006a6ecbb97dc38e9d30ba9062c4024b8c1
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-26 01:29:35 -0500

    Added notifications

--- src-cljs/houserules/messages.cljs
+++ src-cljs/houserules/messages.cljs
@@ -2,18 +2,31 @@
   (:require [reagent.core :as reagent :refer [atom]]))
 
 (def messages (atom []))
+(def notifications (atom []))
 
 (defn messages-area []
-  [:div (for [msg @messages] msg)])
+  [:div#messages (for [msg @messages] msg)])
 
-(defn remove-message [id]
-  (swap! messages (fn [coll] (filter #(not= (:id (meta %)) id) coll))))
+(defn notifications-area []
+  [:div#notifications (for [notif (reverse @notifications)] notif)])
+
+(defn remove-message [coll id]
+  (swap! coll (fn [coll] (filter #(not= (:id (meta %)) id) coll))))
 
 (defn add-message [type header content]
   (let [id (gensym)]
     (swap! messages conj ^{:id id}
                      [(keyword (str "div.ui.message." (name type)))
-                      [:i.close.icon {:on-click #(remove-message id)}]
+                      [:i.close.icon {:on-click #(remove-message messages id)}]
                       (when header
                         [:div.header header])
                       content])))
+
+(defn add-notification [type header content]
+  (let [id (gensym)]
+    (swap! notifications conj ^{:id id}
+                         [(keyword (str "div.ui.compact.message." (name type)))
+                          (when header
+                            [:div.header header])
+                          content])
+    (js/setTimeout #(remove-message notifications id) 3000)))
