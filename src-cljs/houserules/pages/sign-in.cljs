8359371db048d8a545289c6863e7d3068eee172b
Daniel Gagnon <dan@daniel.gagnon.me>
2014-11-26 21:42:58 -0500

    Added password reset message

--- src-cljs/houserules/pages/sign-in.cljs
+++ src-cljs/houserules/pages/sign-in.cljs
@@ -24,15 +24,19 @@
         email-error (atom false)
         password-error (atom false)
         login #(do (send-login @email @password in-flight email-error password-error))
-        clear-errors #(do (reset! email-error false) (reset! password-error false))]
+        clear-errors #(do (reset! email-error false) (reset! password-error false))
+        reset-password-sent (atom true)]
     (fn []
       [:div#sign-in.ui.form
        [:div.ui.attached.message
         [:div.header "Sign in"]]
        [:div.ui.form.attached.fluid.segment
-        [(if-not @email-error :div.field :div.field.error)
-         [:input#email.ui {:value @email :placeholder "Email" :type :text :disabled @in-flight :auto-focus true :on-change #(do (clear-errors) (reset! email (string/trim (-> % .-target .-value))))}]]
-        [(if-not @password-error :div.field :div.field.error)
-         [:input#password.ui {:value @password :placeholder "Password" :type :password :disabled @in-flight :on-change #(do (clear-errors) (reset! password (-> % .-target .-value))) :on-key-down #(when (= 13 (.-keyCode %)) (login))}]]
-        [(keyword (str "button#forgot-password.ui.button" (when (or (not (re-find #".+@.+\..+" @email)) @in-flight) ".disabled"))) {:disabled (or (not (re-find #".+@.+\..+" @email)) @in-flight)} "I forgot my password"]
-        [(if-not (disable-button) :button.ui.primary.button :button.ui.primary.button.disabled) {:disabled (disable-button) :on-click login} "Login"]]])))
+        (if @reset-password-sent
+          [:p "Check your emails for a password reset link."]
+          [:div
+           [(if-not @email-error :div.field :div.field.error)
+            [:input#email.ui {:value @email :placeholder "Email" :type :text :disabled @in-flight :auto-focus true :on-change #(do (clear-errors) (reset! email (string/trim (-> % .-target .-value))))}]]
+           [(if-not @password-error :div.field :div.field.error)
+            [:input#password.ui {:value @password :placeholder "Password" :type :password :disabled @in-flight :on-change #(do (clear-errors) (reset! password (-> % .-target .-value))) :on-key-down #(when (= 13 (.-keyCode %)) (login))}]]
+           [(keyword (str "button#forgot-password.ui.button" (when (or (not (re-find #".+@.+\..+" @email)) @in-flight) ".disabled"))) {:disabled (or (not (re-find #".+@.+\..+" @email)) @in-flight)} "I forgot my password"]
+           [(if-not (disable-button) :button.ui.primary.button :button.ui.primary.button.disabled) {:disabled (disable-button) :on-click login} "Login"]])]])))
