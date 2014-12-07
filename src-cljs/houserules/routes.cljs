5d9d29fe22c0b3972c242823244bbd8d1bb6fdf4
Daniel Gagnon <dan@daniel.gagnon.me>
2014-12-07 14:11:06 -0500

    Sending users from the password reset link to the profile page

--- src-cljs/houserules/routes.cljs
+++ src-cljs/houserules/routes.cljs
@@ -5,6 +5,10 @@
 
 (def current-page (atom nil))
 
+(defn navigate-to [p]
+  (.pushState js/history p "" p)
+  (secretary/dispatch! p))
+
 (defroute home-route "/" [] (reset! current-page :home))
 (defroute admin-route "/admin" [] (reset! current-page :admin))
 (defroute profile-route "/profile" [] (reset! current-page :profile))
@@ -12,10 +16,7 @@
 (defroute register-details-route "/register/:token" [_] (reset! current-page :register-details)) ;; Token is parsed on server
 (defroute sign-in-route "/sign/in" [] (reset! current-page :sign-in))
 (defroute sign-out-route "/sign/out" [] (reset! current-page :sign-out))
-
-(defn navigate-to [p]
-  (.pushState js/history p "" p)
-  (secretary/dispatch! p))
+(defroute reset-password-route "/password-reset/:token" [_] (navigate-to (profile-route)))
 
 (aset js/window "onpopstate"
       (fn [event] (secretary/dispatch! (or (aget event "state") "/"))))
