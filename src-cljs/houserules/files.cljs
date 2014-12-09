cb87ab845db39e1d8080d0e14f2fbf38a84cdb3f
Daniel Gagnon <dan@daniel.gagnon.me>
2014-12-08 23:38:24 -0500

    Store file list in atom

--- src-cljs/houserules/files.cljs
+++ src-cljs/houserules/files.cljs
@@ -1,5 +1,10 @@
 (ns houserules.files
   (:require [reagent.core :refer [atom]]))
 
+(def selected-files (atom nil))
+
 (defn hidden-file-selector [accept]
-  [:input {:type :file, :accept accept :style {:display :none}}])
+  [:input {:type :file
+           :accept accept
+           :style {:display :none}
+           :on-change #(reset! selected-files (-> % .-target .-files))}])
