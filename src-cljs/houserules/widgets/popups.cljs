06ae5948ef44314412ec2b3bb95c90d948819807
Daniel Gagnon <dan@daniel-gagnon.me>
2014-12-10 00:06:19 -0500

    Loading fabric.js before Darkroom

--- src-cljs/houserules/widgets/popups.cljs
+++ src-cljs/houserules/widgets/popups.cljs
@@ -1,17 +1,20 @@
 (ns houserules.widgets.popups
   (:require [reagent.core :as reagent :refer [atom]]
-            [houserules.files :refer [hidden-file-selector]]))
+            [houserules.files :refer [hidden-file-selector]]
+            [houserules.async :refer [darkroom?]]))
 
 (def current-popup (atom nil))
 
 (defn- clear-popup [] (reset! current-popup nil))
 
 (defn crop-popup [title img]
-  [:div.window.ui.stacked.segment
-   [:h2.ui.header title]
-   [:div.content
-    [:div.placeholder {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} "Click to upload a picture or drag and drop one here"]]
-   [hidden-file-selector "images/*"]
-   [:div.actions
-    [:button.ui.primary.button "Ok"]
-    [:button.ui.button {:on-click clear-popup} "Cancel"]]])
+  (let [darkroom (darkroom?)]
+    (println darkroom)
+    [:div.window.ui.stacked.segment
+     [:h2.ui.header title]
+     [:div.content
+      [:div.placeholder {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} "Click to upload a picture or drag and drop one here"]]
+     [hidden-file-selector "images/*"]
+     [:div.actions
+      [:button.ui.primary.button "Ok"]
+      [:button.ui.button {:on-click clear-popup} "Cancel"]]]))
