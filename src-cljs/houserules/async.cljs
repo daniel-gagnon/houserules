1dadf57b79d0e6e52c677f6fb955f572a06821ea
Daniel Gagnon <dan@daniel-gagnon.me>
2014-12-09 23:57:47 -0500

    Moved to script elements because of recaptcha's same-origin's policy

--- src-cljs/houserules/async.cljs
+++ src-cljs/houserules/async.cljs
@@ -6,29 +6,33 @@
 (def ^:private recaptcha (atom nil))
 (def ^:private darkroom (atom nil))
 
-(defn async-load [urls atm]
+(defn wait-for [f atm]
+  (let [interval-key (atom nil)]
+    (reset! interval-key (.setInterval js/window #(when (aget js/window f) (.clearInterval js/window @interval-key) (reset! atm true)) 0))))
+
+(defn async-load [urls atm f]
   (if-let [url (first urls)]
-    (GET url {:handler
-              #(do
-                (js/eval %)
-                (async-load (rest urls) atm))})
-    (reset! atm true)))
+    (let [script-element (.createElement js/document "script")]
+      (.appendChild (.-head js/document) script-element)
+      (aset script-element "onload" #(async-load (rest urls) atm f))
+      (aset script-element "src" url))
+    (wait-for f atm)))
 
 (defn zxcvbn? []
   (when (nil? @zxcvbn)
     (reset! zxcvbn false)
-    (async-load ["/static/javascript/zxcvbn.js"] zxcvbn)
+    (async-load ["/static/javascript/zxcvbn.js"] zxcvbn "zxcvbn")
     @zxcvbn))
 
 
 (defn recaptcha? []
   (when (nil? @recaptcha)
     (reset! recaptcha false)
-    (async-load ["https://www.google.com/recaptcha/api.js?render=explicit"] recaptcha))
+    (async-load ["https://www.google.com/recaptcha/api.js?render=explicit"] recaptcha "grecaptcha"))
   @recaptcha)
 
 (defn darkroom? []
   (when (nil? @darkroom)
     (reset! darkroom false)
-    (async-load ["/static/javascript/darkroom.min.js"] darkroom)
+    (async-load ["/static/javascript/darkroom.min.js"] darkroom "Darkroom")
     @darkroom))
