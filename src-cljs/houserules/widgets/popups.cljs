386efc92fbe35ee665a81e4fc3322f802442480d
Daniel Gagnon <dan@daniel-gagnon.me>
2014-12-14 14:53:36 -0500

    Not enabling the ok button before an image is selected

--- src-cljs/houserules/widgets/popups.cljs
+++ src-cljs/houserules/widgets/popups.cljs
@@ -8,12 +8,13 @@
 (defn- clear-popup [] (reset! current-popup nil))
 
 (defn crop-popup [title img]
-  [:div.window.ui.stacked.segment
-   [:h2.ui.header title]
-   [:div.content
-    (when (darkroom?)
-      [:div.placeholder (merge {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} file-drop-zone) "Click to upload a picture or drag and drop one here"])]
-   [hidden-file-selector "images/*"]
-   [:div.actions
-    [:button.ui.primary.button {:on-click (comp #(reset! img (first @files)) remove-files clear-popup)} "Ok"]
-    [:button.ui.button {:on-click (comp remove-files clear-popup)} "Cancel"]]])
+  (let [first-file-is-image #(re-find #"image/.*" (or (:type (first @files)) ""))]
+    [:div.window.ui.stacked.segment
+     [:h2.ui.header title]
+     [:div.content
+      (when (darkroom?)
+        [:div.placeholder (merge {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} file-drop-zone) "Click to upload a picture or drag and drop one here"])]
+     [hidden-file-selector "images/*"]
+     [:div.actions
+      [(if (first-file-is-image) :button.ui.primary.button :button.ui.primary.button.disabled) {:disabled (not (first-file-is-image)) :on-click (comp #(reset! img (first @files)) remove-files clear-popup)} "Ok"]
+      [:button.ui.button {:on-click (comp remove-files clear-popup)} "Cancel"]]]))
