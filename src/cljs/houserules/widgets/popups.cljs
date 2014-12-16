536c338f997e225b116d98ff4d2ef8ec53d3a092
Daniel Gagnon <dan@daniel-gagnon.me>
2014-12-16 02:23:57 -0500

    Displaying selected picture

--- src/cljs/houserules/widgets/popups.cljs
+++ src/cljs/houserules/widgets/popups.cljs
@@ -15,7 +15,9 @@
      [:h2.ui.header title]
      [:div.content
       (when (darkroom?)
-        [:div.placeholder (merge {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} file-drop-zone) "Click to upload a picture or drag and drop one here"])]
+        (if (first-file-is-image)
+          [:img {:src (:data @first-file) :style {:max-width: "100%", :max-height "100%"}}]
+          [:div.placeholder (merge {:on-click #(.click (.querySelector js/document "div.window input[type=file]"))} file-drop-zone) "Click to upload a picture or drag and drop one here"]))]
      [hidden-file-selector "images/*"]
      [:div.actions
       [(if (first-file-is-image) :button.ui.primary.button :button.ui.primary.button.disabled) {:disabled (not (first-file-is-image)) :on-click (comp #(reset! img (first @files)) remove-files clear-popup)} "Ok"]
