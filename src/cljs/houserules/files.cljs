1897879b5a294a6203185dec9dcb8170291f132b
Daniel Gagnon <dan@daniel-gagnon.me>
2014-12-15 23:35:03 -0500

    Read files as data urls

--- src/cljs/houserules/files.cljs
+++ src/cljs/houserules/files.cljs
@@ -1,42 +1,40 @@
 (ns houserules.files
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(defrecord file [name size type length data])
+(defrecord file [name size type data])
+(defrecord file-progress [name size type loaded])
 
 (def selected-files (atom nil))
 (def files (atom nil))
 (defn remove-files [] (reset! selected-files nil))
 
-(defn revoke-objects-urls [objects]
-  (doseq [o objects] ((aget js/window "URL" "revokeObjectURL") o)))
-
-(defn create-objects-urls [objects]
-  (->> objects
-       (map (juxt
-              #(aget % "name")
-              #(aget % "size")
-              #(aget % "type")
-              #(aget % "length")
-              (aget js/window "URL" "createObjectURL")))
-       (map #(apply ->file %))))
+(defn read-file [f]
+  (let [name (aget f "name")
+        size (aget f "size")
+        type (aget f "type")
+        reader (js/FileReader.)
+        atm (atom (->file-progress name size type 0))]
+    (aset reader "onprogress" #(swap! atm assoc :loaded (aget % "loaded")))
+    (aset reader "onload" #(reset! atm (->file name size type (aget reader "result"))))
+    (.readAsDataURL reader f)
+    atm))
 
 (add-watch selected-files (gensym)
-  (fn [_ _ old new]
-    (revoke-objects-urls old)
-    (reset! files (create-objects-urls new))))
+  (fn [_ _ _ new]
+    (reset! files (mapv read-file new))))
+
+(defn filelist->vector [filelist]
+  (vec (for [idx (range (aget filelist "length"))] (.item filelist idx))))
 
 (defn hidden-file-selector [accept]
   [:input {:type :file
            :accept accept
            :style {:display :none}
-           :on-change #(reset! selected-files (-> % .-target .-files))}])
+           :on-change #(reset! selected-files (-> % .-target .-files filelist->vector))}])
 
 (defn prevent-default [e]
   (.stopPropagation e)
   (.preventDefault e))
-
-(defn filelist->vector [filelist]
-  (for [idx (range (aget filelist "length"))] ((aget filelist "item") idx)))
 
 (def file-drop-zone
   {:on-drop #(do (prevent-default %)
