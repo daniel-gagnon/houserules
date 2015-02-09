3a8d7e52b6a6484e2e215742285ba98f91e00c56
Daniel Gagnon <dan@daniel-gagnon.me>
2015-02-08 21:46:47 -0500

    Added exclusions to fix deps conflits

--- project.clj
+++ project.clj
@@ -20,34 +20,34 @@
    [org.clojure/clojurescript "0.0-2760"]
    [org.clojure/clojure "1.6.0"]
    [environ "1.0.0"]
-   [ring-server "0.3.1"]
+   [ring-server "0.4.0"]
    [secretary "1.2.1"]
    [im.chit/cronj "1.4.3"]
    [org.clojure/data.json "0.2.5"]
    [com.sleepycat/je "6.2.7"]
-   [com.taoensso/nippy "2.7.1"]
+   [com.taoensso/nippy "2.7.1" :exclusions [com.taoensso/encore org.clojure/tools.reader]]
    [slingshot "0.12.1"]
    [org.clojure/tools.trace "0.7.8"]
    [joda-time/joda-time "2.7"]
    [clj-yaml "0.4.0"]
-   [markdown-clj "0.9.58" :exclusions [com.keminglabs/cljx]]
+   [markdown-clj "0.9.62" :exclusions [com.keminglabs/cljx]]
    [com.draines/postal "1.11.3"]
    [reagent-utils "0.1.1"]
-   [org.clojure/clojurescript "0.0-2371" :scope "provided"]
-   [com.cemerick/piggieback "0.1.3"]
-   [weasel "0.4.0-SNAPSHOT"]
-   [figwheel "0.1.5-SNAPSHOT"]
-   [leiningen "2.5.0"]]
+   [org.clojure/clojurescript "0.0-2760" :scope "provided"]
+   [com.cemerick/piggieback "0.1.5"]
+   [weasel "0.5.0"]
+   [figwheel "0.2.3-20150208.002609-4" :exclusions [org.clojure/core.async]]
+   [leiningen "2.5.1"]]
   :repl-options
   {:init-ns houserules.repl}
   :jvm-opts
   ["-server"]
   :repositories [["Oracle" "http://download.oracle.com/maven/"]]
   :plugins
-  [[lein-ring "0.8.13"]
+  [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]
    [lein-environ "1.0.0"]
-   [lein-ancient "0.5.5"]
-   [lein-cljsbuild "1.0.3"]]
+   [lein-ancient "0.5.5" :exclusions [org.clojure/clojure org.clojure/clojure org.clojure/data.xml]]
+   [lein-cljsbuild "1.0.3" :exclusions [org.clojure/clojure]]]
   :ring
   {:handler houserules.handler/app,
    :init houserules.handler/init,
@@ -55,10 +55,10 @@
   :profiles {:dev {:repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
                    :dependencies [[ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.3.1"]
+                                  [ring/ring-devel "1.3.2"]
                                   [pjstadig/humane-test-output "0.6.0"]]
 
-                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
+                   :plugins [[lein-figwheel "0.2.1-SNAPSHOT" :exclusions [org.clojure/clojure org.clojure/clojure org.clojure/clojurescript]]]
 
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
